71289e194accec5a72eecc7792759069efdd2403
Karthik-CT <karthik.iyer@clevertap.com>
2025-09-07 18:06:44 +0530

    updated library

--- CTTemplates/Sources/CTTemplates/TooltipManager.swift
+++ CTTemplates/Sources/CTTemplates/TooltipManager.swift
@@ -120,16 +120,23 @@ public class TooltipManager {
 
 extension UIColor {
     convenience init?(ct_hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        var formatted = ct_hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if formatted.hasPrefix("#") { formatted.removeFirst() }
         
         var rgb: UInt64 = 0
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+        guard Scanner(string: formatted).scanHexInt64(&rgb) else { return nil }
         
-        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255
-        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255
-        let b = CGFloat(rgb & 0x0000FF) / 255
-        
-        self.init(red: r, green: g, blue: b, alpha: 1.0)
+        switch formatted.count {
+        case 6:
+            self.init(
+                red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgb & 0x0000FF) / 255.0,
+                alpha: 1.0
+            )
+        default:
+            return nil
+        }
     }
 }
+
