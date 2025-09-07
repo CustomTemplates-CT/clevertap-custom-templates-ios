772a694cc8255cb8f5dc632826276e0543093f36
Karthik-CT <karthik.iyer@clevertap.com>
2025-09-07 19:10:34 +0530

    updated library

--- CTTemplates/Sources/CTTemplates/SpotlightView.swift
+++ CTTemplates/Sources/CTTemplates/SpotlightView.swift
@@ -156,27 +156,17 @@ class Spotlight {
         showStep()
     }
     
+    @MainActor
     func showStep() {
         guard currentIndex < steps.count else { return }
         let step = steps[currentIndex]
-        
-//        if let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) {
-//            let spotlight = SpotlightView(step: step)
-//            spotlight.onDismiss = {
-//                currentIndex += 1
-//                showStep()
-//            }
-//            window.addSubview(spotlight)
-//        }
-        Task { @MainActor in
-            if let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) {
-                let spotlight = SpotlightView(step: step)
-                spotlight.onDismiss = { [weak self] in
-                    self?.currentIndex += 1
-                    self?.showStep()
-                }
-                window.addSubview(spotlight)
+        if let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) {
+            let spotlight = SpotlightView(step: step)
+            spotlight.onDismiss = {
+                currentIndex += 1
+                showStep()
             }
+            window.addSubview(spotlight)
         }
     }
     
